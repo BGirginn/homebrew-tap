821e9e852011f9efab7f5414fc48954dbac4163a
BGirgin <girginbora30@gmail.com>
2026-08-12 16:35:31 +0300

    chore: update ClipboardHistory to 1.0.0-beta.3

--- Casks/clipboardhistory.rb
+++ Casks/clipboardhistory.rb
@@ -1,11 +1,15 @@
 cask "clipboardhistory" do
-  version "1.0.0-beta.2"
-  sha256 "b4d826e42554f2a7275c7370f86caacdede3a98cc45cc8bedcc0c590ad37c37b"
+  version "1.0.0-beta.3"
+  sha256 "48f3c8ecf1400a24730053d64e81a8c5f076fca12060059e25526032cee2e7b0"
 
   url "https://github.com/BGirginn/ClipboardHistory/releases/download/v#{version}/ClipboardHistory-#{version}-arm64.zip"
   name "ClipboardHistory"
-  desc "Local clipboard history manager with encryption and privacy controls"
+  desc "Local menu-bar utility hub with clipboard history and notes"
   homepage "https://github.com/BGirginn/ClipboardHistory"
+
+  livecheck do
+    skip "Only Community beta releases are published"
+  end
 
   depends_on arch: :arm64
   depends_on macos: :sonoma
